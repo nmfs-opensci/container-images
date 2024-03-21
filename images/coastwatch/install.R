b57b8b6ab80b0cfcd57d60f53a21aa501a740841
Eli Holmes <eeholmes@uw.edu>
2024-03-21 14:18:12 -0700

    pin a repository to use. to use the one in the rocker image

--- images/coastwatch/install.R
+++ images/coastwatch/install.R
@@ -1,10 +1,20 @@
 #! /usr/local/bin/Rscript
 # install R dependencies
 
-list.of.packages <- c("ncdf4", "httr","plyr","lubridate", "parsedate", "rerddap","plotdap",
-                      "rerddapXtracto", "maps", "mapdata","RColorBrewer",
-                      "ggplot2","scales","dplyr","Rcurl","raster","colorRamps",
-                      "parsedate","sp","sf","reshape2","jsonlite",
-                      "gridGraphics","PBSmapping","date","viridis",
-                      "openair","cmocean", "terra")
-install.packages(list.of.packages)
+# to match rocker/geospatial:4.2 uses in openscapes/rocker:a7596b5
+repo <- "https://p3m.dev/cran/__linux__/jammy/2023-04-20"
+list.of.packages <- c("ncdf4", "httr", "plyr", "lubridate", "parsedate", "rerddap",
+                      "maps", "mapdata", "RColorBrewer",
+                      "ggplot2","scales", "dplyr", "Rcurl", "raster", "colorRamps",
+                      "parsedate", "sp", "sf", "reshape2", "jsonlite",
+                      "gridGraphics", "PBSmapping", "date", "viridis",
+                      "openair","cmocean", "terra",
+                      "plotdap", "rerddapXtracto")
+install.packages(list.of.packages, repos=repo)
+
+# https://rmendels.r-universe.dev/plotdap  
+#install.packages('plotdap', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
+
+# https://rmendels.r-universe.dev/rerddapXtracto   
+#install.packages('rerddapXtracto', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
+
