3a75c96b7f365b3c0ec3d77a03db915c5aea91f5
PolarWatch <119896595+shospital@users.noreply.github.com>
2024-03-21 13:34:45 -0700

    Update install.R
    added install.packages with repos source for rerddapXtract and plotdap

--- images/coastwatch/install.R
+++ images/coastwatch/install.R
@@ -1,10 +1,16 @@
 #! /usr/local/bin/Rscript
 # install R dependencies
 
-list.of.packages <- c("ncdf4", "httr","plyr","lubridate", "parsedate", "rerddap","plotdap",
-                      "rerddapXtracto", "graphics", "maps", "mapdata","RColorBrewer",
+list.of.packages <- c("ncdf4", "httr","plyr","lubridate", "parsedate", "rerddap",
+                      "graphics", "maps", "mapdata","RColorBrewer",
                       "ggplot2","scales","dplyr","utils","Rcurl","raster","colorRamps",
                       "parsedate","sp","sf","reshape2","jsonlite","rgdal",
                       "gridGraphics","grid","PBSmapping","date","viridis",
                       "openair","cmocean", "terra")
 install.packages(list.of.packages)
+
+# https://rmendels.r-universe.dev/rerddapXtracto   
+install.packages('rerddapXtracto', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
+
+# https://rmendels.r-universe.dev/plotdap  
+install.packages('plotdap', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
