122cabbf02483bd1efdf04b03d9966085dd836e6
Eli Holmes <eeholmes@uw.edu>
2024-03-21 14:28:52 -0700

    RCurl not Rcurl

--- images/coastwatch/install.R
+++ images/coastwatch/install.R
@@ -5,16 +5,9 @@
 repo <- "https://p3m.dev/cran/__linux__/jammy/2023-04-20"
 list.of.packages <- c("ncdf4", "httr", "plyr", "lubridate", "parsedate", "rerddap",
                       "maps", "mapdata", "RColorBrewer",
-                      "ggplot2","scales", "dplyr", "Rcurl", "raster", "colorRamps",
+                      "ggplot2","scales", "dplyr", "RCurl", "raster", "colorRamps",
                       "parsedate", "sp", "sf", "reshape2", "jsonlite",
                       "gridGraphics", "PBSmapping", "date", "viridis",
                       "openair","cmocean", "terra",
-                      "plotdap", "rerddapXtracto")
+                      "plotdap", "rerddapXtracto", "rgdal")
 install.packages(list.of.packages, repos=repo)
-
-# https://rmendels.r-universe.dev/plotdap  
-#install.packages('plotdap', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
-
-# https://rmendels.r-universe.dev/rerddapXtracto   
-#install.packages('rerddapXtracto', repos = c('https://rmendels.r-universe.dev', 'https://cloud.r-project.org'))
-
