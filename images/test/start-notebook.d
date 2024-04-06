08bcc934e5825f627c1967b74d3353ef1b984268
Eli Holmes <eli.holmes@noaa.gov>
2024-04-06 09:34:36 -0700

    exp

--- images/test/start-notebook.d
+++ images/test/start-notebook.d
@@ -1,4 +1,20 @@
-KubeSpawner.cmd = ['start-notebook.sh']
-c.Spawner.cmd = ["jupyter-labhub"]
-export JUPYTERHUB_SINGLEUSER_APP='jupyter_server.serverapp.ServerApp'
-c.Spawner.default_url = '/lab/'
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -e
+
+wrapper=""
+if [[ "${RESTARTABLE}" == "yes" ]]; then
+    wrapper="run-one-constantly"
+fi
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+    # launched by JupyterHub, use single-user entrypoint
+    exec /usr/local/bin/start-singleuser.sh "$@"
+elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+    . /usr/local/bin/start.sh $wrapper jupyter lab "$@"
+else
+    echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
+    . /usr/local/bin/start.sh $wrapper jupyter notebook "$@"
+fi
