1970eaec111bd4e836b614d9a0ffa9316a30d0ef
Eli Holmes <eli.holmes@noaa.gov>
2024-04-06 12:40:59 -0700

    uncomment things

--- images/test/jupyter_server_config.py
+++ images/test/jupyter_server_config.py
@@ -11,10 +11,10 @@ c = get_config()  # noqa: F821
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
-#c.Spawner.cmd = ["jupyter-labhub"]
-#c.Spawner.default_url = '/lab/'
-#c.SingleUserNotebookApp.default_url = "/lab/"
-#c.NotebookApp.default_url = "/lab/"
+c.Spawner.cmd = ["jupyter-labhub"]
+c.Spawner.default_url = '/lab/'
+c.SingleUserNotebookApp.default_url = "/lab/"
+c.NotebookApp.default_url = "/lab/"
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
