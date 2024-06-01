6d78ff88352f3770542b624fe2b45ad01bc4de6c
Eli Holmes - NOAA <eeholmes@uw.edu>
2024-06-01 01:56:33 -0400

    Update post_start.sh

--- .devcontainer/codespace/post_start.sh
+++ .devcontainer/codespace/post_start.sh
@@ -7,4 +7,13 @@ echo "Debug: Constructed message."
 # Echo the message to the terminal
 echo "$message"
 
+# Check if the message is already in README.md and append if it's not
+echo "Debug: Checking if message is in README.md."
+if ! grep -Fqx "$message" /home/jovyan/codespace/README.md; then
+    echo "Debug: Message not found in README.md, appending now."
+    echo "$message" >> /home/jovyan/codespace/README.md
+else
+    echo "Debug: Message already in README.md, not appending."
+fi
+
 sleep 10
