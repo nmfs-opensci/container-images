26636336ae3e804cf2625f9569bafaa6b4ef4a41
Eli Holmes - NOAA <eeholmes@uw.edu>
2024-06-01 02:24:57 -0400

    Update post_start.sh

--- .devcontainer/codespace/post_start.sh
+++ .devcontainer/codespace/post_start.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # Construct the message
-message="## [Open Jupyter Lab](https://$CODESPACE_NAME-8889.app.github.dev)"
+message="# Welcome
+- This VS code container is the entry to your Jupyter Lab
+- This Codespace is not connected to a repo. Make sure to connect to a GitHub repo and push your changes.
+  
+# Jupyter Lab is available at this URL:
+## [Open Jupyter Lab](https://$CODESPACE_NAME-8889.app.github.dev)"
 echo "Debug: Constructed message."
 
-# Echo the message to the terminal
-echo "$message"
-
-# Check if the message is already in README.md and append if it's not
-echo "Debug: Checking if message is in README.md."
-if ! grep -Fqx "$message" /home/jovyan/README.md; then
-    echo "Debug: Message not found in README.md, appending now."
-    echo "$message" >> /home/jovyan/codespace/README.md
-else
-    echo "Debug: Message already in README.md, not appending."
-fi
+echo "$message" > /home/jovyan/README.md
 
 # Start Jupyter Lab on port 8889
 jupyter lab --ip=0.0.0.0 --port=8889 --allow-root --no-browser --NotebookApp.token='' --NotebookApp.password=''
