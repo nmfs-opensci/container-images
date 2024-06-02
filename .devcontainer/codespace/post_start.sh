21207c68288f5fbc35dbc6388118e22dd52884eb
Eli Holmes <eli.holmes@noaa.gov>
2024-06-01 20:43:04 -0700

    need rstudio password

--- .devcontainer/codespace/post_start.sh
+++ .devcontainer/codespace/post_start.sh
@@ -21,7 +21,7 @@ sleep 5
 
 nohup jupyter lab --ip=0.0.0.0 --port=8889 --allow-root --no-browser --NotebookApp.token='' --NotebookApp.password='' >/dev/null 2>&1 &
 
-sudo apt-get update
+echo rstudio | sudo -S apt-get update
 sudo apt-get install -y xdg-utils
 
 xdg-open https://$CODESPACE_NAME-8889.app.github.dev
