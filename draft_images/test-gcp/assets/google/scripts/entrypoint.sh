e6921197ee55d6e58979e5b059eb4400703a9530
Eli Holmes <eli.holmes@noaa.gov>
2024-07-03 11:01:09 -0700

    change user for entrypoint cmd

--- draft_images/test-gcp/assets/google/scripts/entrypoint.sh
+++ draft_images/test-gcp/assets/google/scripts/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Starting entrypoint.sh"
 
 echo "Starting Jupyter Lab in background"
-runuser user -c "jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''" &
+runuser jovyan -c "jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''" &
 echo "Blocking indefinitely"
-runuser user -c "sleep infinity"
+runuser jovyan -c "sleep infinity"
 
 echo "Exiting entrypoint.sh"
