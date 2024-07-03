77e09c1076bdb3bd47d1d59832b725145357569c
Eli Holmes <eli.holmes@noaa.gov>
2024-07-03 15:03:06 -0700

    back to 80

--- draft_images/test-gcp/assets/google/scripts/entrypoint.sh
+++ draft_images/test-gcp/assets/google/scripts/entrypoint.sh
@@ -3,8 +3,7 @@
 echo "Starting entrypoint.sh"
 
 echo "Starting Jupyter Lab in background"
-jupyter lab --ip=0.0.0.0 --port=8889 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password='' &
-#jupyter server --ServerApp.port=80 &
+jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password='' &
 
 echo "Blocking indefinitely"
 sleep infinity
