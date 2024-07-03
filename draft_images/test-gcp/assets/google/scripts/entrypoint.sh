217bb5949b022695f226aeab2daf8c6d3bc3dfe1
Eli Holmes <eli.holmes@noaa.gov>
2024-07-03 11:10:58 -0700

    get rid of runuser cmd

--- draft_images/test-gcp/assets/google/scripts/entrypoint.sh
+++ draft_images/test-gcp/assets/google/scripts/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Starting entrypoint.sh"
 
 echo "Starting Jupyter Lab in background"
-runuser rstudio -c "jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''" &
+jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''
 echo "Blocking indefinitely"
-runuser rstudio -c "sleep infinity"
+sleep infinity
 
 echo "Exiting entrypoint.sh"
