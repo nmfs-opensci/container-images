1061cb177233a7355dc3e1bb0ad5fec6f44a5961
Eli Holmes <eli.holmes@noaa.gov>
2024-07-06 15:30:07 -0700

    try enabling jupyter server extension

--- draft_images/test-gcp/assets/google/scripts/entrypoint.sh
+++ draft_images/test-gcp/assets/google/scripts/entrypoint.sh
@@ -3,10 +3,13 @@
 
 echo "Starting entrypoint.sh"
 
+echo 'Enable jupyter serverextension'
+sudo jupyter serverextension enable --py jupyterlab --sys-prefix
+
 echo "Starting Jupyter Lab in background"
-runuser rstudio -c "jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''" &
+jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password='' &
 
 echo "Blocking indefinitely"
-runuser rstudio -c "sleep infinity"
+sleep infinity
 
 echo "Exiting entrypoint.sh"
