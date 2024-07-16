63dce099ee2687cd9c88231e5776a851fd0f0ea6
Eli Holmes <eli.holmes@noaa.gov>
2024-07-16 15:50:18 -0700

    v1.0.16 forgot sleep infinity

--- draft_images/test-gcp/assets/google/scripts/entrypoint.sh
+++ draft_images/test-gcp/assets/google/scripts/entrypoint.sh
@@ -6,7 +6,7 @@ echo "Starting entrypoint.sh"
 echo "Starting Jupyter Lab in background"
 jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser --notebook-dir=/home/jovyan --NotebookApp.token='' --NotebookApp.password=''  --NotebookApp.allow_origin='*' &
 
-#echo "Blocking indefinitely"
-#sleep infinity
+echo "Blocking indefinitely"
+sleep infinity
 
 echo "Exiting entrypoint.sh"
