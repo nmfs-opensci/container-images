6258f1a5769cfd658bb34673acf4440b50eb14cf
Eli Holmes <eli.holmes@noaa.gov>
2024-10-19 10:31:04 -0700

    initial commit

---
+++ draft_images/coastwatch-qgis/install.R
@@ -0,0 +1,14 @@
+#! /usr/local/bin/Rscript
+# install R dependencies
+
+# to match rocker/verse:4.4 used in py-rocker-base
+# look up the date that the Rocker image was created and put that
+repo <- "https://p3m.dev/cran/__linux__/jammy/2024-05-13"
+list.of.packages <- c("ncdf4", "httr", "plyr", "lubridate", "parsedate", "rerddap",
+                      "maps", "mapdata", "RColorBrewer",
+                      "ggplot2","scales", "dplyr", "RCurl", "raster", "colorRamps",
+                      "parsedate", "sp", "sf", "reshape2", "jsonlite",
+                      "gridGraphics", "PBSmapping", "date", "viridis",
+                      "openair","cmocean", "terra",
+                      "plotdap", "rerddapXtracto")
+install.packages(list.of.packages, repos=repo)
