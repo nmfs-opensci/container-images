0aeb4fe55556d21ab46d9b542e458c65e4d162af
Eli Holmes <eli.holmes@noaa.gov>
2025-02-05 20:31:21 -0800

    update to newest py-rocket-base
    * fix texlive install

--- images/py-rocket-geospatial-2/install.R
+++ images/py-rocket-geospatial-2/install.R
@@ -2,8 +2,8 @@
 # install R dependencies
 
 # to match rocker/verse:4.4 used in py-rocker-base
-# look up the date that the Rocker image was created and put that
-repo <- "https://p3m.dev/cran/__linux__/jammy/2024-05-13"
+# look up the CRAN env set in the Dockerfile used
+repo <- "https://p3m.dev/cran/__linux__/jammy/2024-10-30"
 
 install.packages(c("rstac", "quarto", "aws.s3", "reticulate", "gdalcubes", "rnaturalearth"), repos=repo)
 install.packages("rnaturalearthdata", repos=repo)
