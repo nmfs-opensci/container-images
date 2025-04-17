7b6f3ca6460f2e753f0903ff22645e29162673e9
Eli Holmes <eli.holmes@noaa.gov>
2025-04-16 18:53:27 -0700

    add devtools

--- images/asar/install.R
+++ images/asar/install.R
@@ -1,14 +1,14 @@
 #! /usr/local/bin/Rscript
 # install R dependencies
 
-devtools::install_github("nmfs-ost/asar")
-devtools::install_github("nmfs-ost/stockplotr")
-
 # to match rocker/verse:4.4 used in py-rocker-base
 # look up the date that the Rocker image was created
 repo <- "https://p3m.dev/cran/__linux__/jammy/2025-04-10"
 
 # Extra packages
-list.of.packages <- c("quarto", "here", "tinytex")
+list.of.packages <- c("quarto", "here", "tinytex", "devtools")
 install.packages(list.of.packages, repos=repo)
+
+devtools::install_github("nmfs-ost/asar")
+devtools::install_github("nmfs-ost/stockplotr")
 
