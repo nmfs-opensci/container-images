e0ca02d87436b35bcc0a79dc3b0851a88cbce492
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2025-06-18 17:13:58 -0700

    Update install.R

--- images/asar/install.R
+++ images/asar/install.R
@@ -1,6 +1,12 @@
 #! /usr/local/bin/Rscript
 # install R dependencies
 
+# Check if the first library path is inside /home
+install_lib <- .libPaths()[1]
+if (grepl("^/home", install_lib)) {
+  stop("Error: Packages are being installed to /home, which will be removed in the final image. Exiting.", call. = FALSE)
+}
+
 # to match rocker/verse:4.4 used in py-rocker-base
 # look up the date that the Rocker image was created
 repo <- "https://p3m.dev/cran/__linux__/jammy/2025-04-10"
