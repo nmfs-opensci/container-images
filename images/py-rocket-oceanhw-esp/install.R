0a7f28341558d1c965b4364fd97eac34ce7b0f7d
Emilio Mayorga <emiliomayorga@gmail.com>
2025-11-10 12:11:49 -0800

    ohwe: Lots of progress customizing R environment, but a couple of problems remain

--- images/py-rocket-oceanhw-esp/install.R
+++ images/py-rocket-oceanhw-esp/install.R
@@ -11,8 +11,7 @@ if (grepl("^/home", install_lib)) {
   stop("Error: Packages are being installed to /home, which will be removed in the final image. Exiting.", call. = FALSE)
 }
 
-install.packages(c("rstac", "quarto", "aws.s3", "reticulate", "gdalcubes", "rnaturalearth"), repos=repo)
-install.packages("rnaturalearthdata", repos=repo)
+install.packages(c("rstac", "quarto", "aws.s3", "reticulate", "gdalcubes", "rnaturalearth", "rnaturalearthdata"), repos=repo)
 
 remotes::install_github('r-tmap/tmap', upgrade=FALSE)
 # CRAN version is out of date
@@ -20,5 +19,37 @@ devtools::install_github("boettiger-lab/earthdatalogin")
 
 # CoastWatch required
 list.of.packages <- c("parsedate", "reshape2", "gridGraphics", "PBSmapping",   
-                      "date", "openair", "cmocean", "plotdap", "rerddapXtracto")
+                      "date", "cmocean", "plotdap", "rerddapXtracto")
 install.packages(list.of.packages, repos=repo)
+
+# From OceanHackWeek R image
+# TODO: Group into high-level (eg, sfnetworks, oce, robis) vs low-level and narrow (eg, akima, mda)
+list.of.packages_ohw <- c(
+    "plot.matrix", "isdparser", "geonames", "readHAC", "noaa",
+    "greybox", "sfnetworks", "smooth", "oce", "ocedata",
+    "akima", "ape", "biomod2", "caret", "caTools", "CircStats", "corrplot", "cowplot", 
+    "dismo", "doParallel", "earth", "fields", "forecast", "ggspatial", "glmnet", "hexbin",
+    "kableExtra", "latticeExtra", "lmtest", "mapplots", "marmap", "matrixStats", "mda", "metR", 
+    "palmerpenguins", "plotly", "rastervis", "robis", "rosm", "tseries", "tsibble", "udunits2", "urca", "vioplot"
+)
+install.packages(list.of.packages_ohw, repos=repo)
+
+# TODO: Should newer packages be installed *without* "repos=repo"? Or with a newer repo version?
+
+# TODO: Looks like "r-rnoaa" referred to the "noaa" package, https://cran.r-project.org/package=noaa
+# But "noaa" didn't install (11/9)  either, even though it's actively maintained (vers 1.0, 2025-05-14)
+# Could it be that it's just not in "repo"?
+
+# TODO: rastervis was not installed (11/9)
+# It was last updated on 2025-09-01
+
+# TODO: rgdal was not installed (11/9)
+# Should we install "gdalraster"?
+
+
+# Should they include upgrade=FALSE?
+remotes::install_github("hvillalo/echogram")
+remotes::install_github("hvillalo/periods")
+remotes::install_github("hvillalo/satin")
+remotes::install_github("hadley/emo")
+remotes::install_github("JorGarMol/VoCC")
