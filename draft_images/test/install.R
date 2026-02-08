d109e7d0bad60c34820dea0582a595ffb19c0701
Eli Holmes <eli.holmes@noaa.gov>
2026-02-07 23:48:25 -0800

    Update R dependencies and package installations
    Updated CRAN repository URL and modified package installation. Added 'rnaturalearthdata' and 'openair' packages.

--- draft_images/test/install.R
+++ draft_images/test/install.R
@@ -3,7 +3,7 @@
 
 # to match rocker/verse:4.4 used in py-rocker-base
 # look up the CRAN env set in the Dockerfile used
-repo <- "https://p3m.dev/cran/__linux__/jammy/2025-04-10"
+repo <- "https://p3m.dev/cran/__linux__/noble/2025-10-30"
 
 # Check if the first library path is inside /home
 install_lib <- .libPaths()[1]
@@ -11,13 +11,16 @@ if (grepl("^/home", install_lib)) {
   stop("Error: Packages are being installed to /home, which will be removed in the final image. Exiting.", call. = FALSE)
 }
 
-install.packages(c("rstac", "quarto", "aws.s3", "reticulate", "gdalcubes", "rnaturalearth", "rnaturalearthdata"), repos=repo)
+install.packages(c("rstac", "quarto", "aws.s3", "reticulate", "gdalcubes", "rnaturalearth"), repos=repo)
+install.packages("rnaturalearthdata", repos=repo)
 
+remotes::install_github('r-tmap/tmap', upgrade=FALSE)
 # CRAN version is out of date
-devtools::install_github("boettiger-lab/earthdatalogin", upgrade=FALSE)
+devtools::install_github("boettiger-lab/earthdatalogin")
 
 # CoastWatch required
-list.of.packages <- c("parsedate", "reshape2", "gridGraphics", "PBSmapping", "date", "cmocean", "plotdap", "rerddapXtracto")
+list.of.packages <- c("parsedate", "reshape2", "gridGraphics", "PBSmapping",   
+                      "date", "openair", "cmocean", "plotdap", "rerddapXtracto")
 install.packages(list.of.packages, repos=repo)
 
 # -------  OceanHackWeek R image customizations ------- 
